7a26514c25c163a8cf8f2a8e9deda2f41b549f73
fliw <fliw@digitalkode.com>
2020-06-10 15:44:34 +0700

    revisi abstraksi

---
+++ abstraction/abstract_class/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace abstract_class
+{
+    public abstract class universitas{
+        public abstract string rektor {get;}
+        public abstract string metode_kuliah {get;}
+        public abstract void kuliah();
+    }
+    public class amikom : universitas {
+        public override string rektor {get{return "pak yanto";}}
+        public override string metode_kuliah {get{return "online";}}
+        public override void kuliah(){
+            Console.WriteLine("Halo Gaes, Welkom Back To Amikom, Kampus Ungu Kebanggaan Kita, dengan rektor " + this.rektor + " serta metode kuliah " + metode_kuliah);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            universitas Kampus_ungu = new amikom();
+            Kampus_ungu.kuliah();
+        }
+    }
+}
---
+++ abstraction/interface/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace interfaces
+{
+    public interface universitas{
+        string rektor {get;}
+        string metode_kuliah {get;}
+        void kuliah();
+    }
+    public class amikom : universitas {
+        public string rektor {get{return "pak yanto";}}
+        public string metode_kuliah {get{return "online";}}
+        public  void kuliah(){
+            Console.WriteLine("Halo Gaes, Welkom Back To Amikom, Kampus Ungu Kebanggaan Kita, dengan rektor " + this.rektor + " serta metode kuliah " + metode_kuliah);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            universitas Kampus_ungu = new amikom();
+            Kampus_ungu.kuliah();
+        }
+    }
+}
